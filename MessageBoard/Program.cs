f010380b4d82712c01658bc848e8118d355405bb
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-25 01:15:27 +0530

    [Kriti] Refactor. New master message added

--- MessageBoard/Program.cs
+++ MessageBoard/Program.cs
@@ -7,6 +7,7 @@ namespace MessageBoard
         static void Main(string[] args)
         {
             Console.WriteLine("Hello demobranch, this is your own message.");
+            Console.WriteLine("This is added for merging master in branch");
         }
     }
 }
