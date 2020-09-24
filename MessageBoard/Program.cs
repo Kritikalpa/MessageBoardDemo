db1f3fab46460889eab72f60db74748a52bf2201
Kriti Kalpa <krtklpsh@gmail.com>
2020-09-24 19:21:01 +0530

    first commit

---
+++ MessageBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessageBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello master, this is your own message.");
+        }
+    }
+}
